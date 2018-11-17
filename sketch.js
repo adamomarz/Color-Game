807b377cd848294f754d9e8249c92ac1a3bb39a5
adamomarz <marzilli.adam@gmail.com>
2018-11-16 20:06:29 -0500

    fixed ui, and timer added start button

--- sketch.js
+++ sketch.js
@@ -6,36 +6,36 @@ var pickedColor;
 var colorText;
 //Setting varible of the user input
 var input;
+var on = false;
 
 
 function setup() {
-	createCanvas(700, 700);
+	createCanvas(800, 800);
 	//Creating an input field for the numbers
 	input = createInput();
-	input.position(335,600);
+	input.position(915,650);
 	//button to change colors for testing sake
-	var button = createButton("Change");
+	var button = createButton("Start");
 
 	button.mousePressed(colorchange);
 	input.changed(validateColor);
-	r = random(255),
-	g = random(255),
-	b = random(255)
+
+
 }
 
 
 function draw() {
-	background(255,255,255);
+	background(230);
 	textSize(32);
-	text('' + Math.floor(r), 50, 50);
-	fill(255, 255, 255);
-	fill(r, g, b);
-	rect(height/2, width/2, 150, 150);
+	fill(51);
+	fill(r,g,b);
+	rect(400, 400, 150, 150);
 	baseUI();
 }
 
 
 function colorchange(){
+	on = true;
 	r = random(255);
   g = random(255);
   b = random(255);
@@ -54,16 +54,17 @@ function colorchange(){
 	 colorText = "Blue";
 	}
 	console.log(Math.floor(pickedColor));
+
 }
 
 
 function validateColor() {
 
-	if (input.value() == Math.floor(pickedColor) || input.value() <= Math.floor(pickedColor)+20 || input.value() <= Math.floor(pickedColor)-20){
+	if (input.value() == Math.floor(pickedColor) && on == true || input.value() <= Math.floor(pickedColor)+20 || input.value() <= Math.floor(pickedColor)-20){
 			console.log("winner")
 			colorchange();
 			points = points + 1;
-	} else {
+	} else if (on == true) {
 		colorchange();
 		console.log("loser")
 	}
@@ -72,16 +73,15 @@ function validateColor() {
 
 //Base UI contains the clock, points and other things that will be shown to the user
 function baseUI(){
-	text("What is the " + colorText + " value of this color?",100,300)
-	text("R: " + Math.floor(r), 400, 400);
-	text("G: " + Math.floor(g), 400, 440);
-	text("B: " + Math.floor(b), 400, 480);
+	text("What is the " + colorText + " value of this color?",150,300);
 	text("Timer: " + timer, 550, 350);
 	text("Points: " + points, 200, 350);
-	if (frameCount % 60 == 0 && timer > 0) {
+	if (frameCount % 60 == 0 && timer > 0 && on) {
     timer --;
   }
   if (timer == 0) {
     text("GAME OVER", width/2, height*0.7);
+		on = false;
+		input.disabled();
   }
 }
